ddf0a9cc2ba282dcff5af3f4e775a5f8a473a852
adminph-de <patrick.hayo@icloud.com>
2021-10-15 09:36:21 +0200

    initial commit

---
+++ deloy-jenkins-container.sh
@@ -0,0 +1,16 @@
+
+# create Jenkins Container
+docker run --name jenkins -p 8080:8080 -p 50000:50000 -d -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+
+docker run --name jenkins -p 8080:8080 -p 50000:50000 -d -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home jenkins-master:alpha-1.0
+
+# install DOCKER
+docker exec -u root -t jenkins apt-get update
+docker exec -u root -t jenkins apt-get install docker.io
+
+# add local DOCKER SOCKET to the container
+docker exec --user root jenkins chmod 777 /var/run/docker.sock
+
+# Deploy NOVO NORDISK zSCALER Cerificate
+docker cp /usr/local/share/ca-certificates/ZscalerRootCertificate-2048-SHA256.crt jenkins:/usr/local/share/ca-certificates/
+docker exec -u root -t jenkins update-ca-certificates
